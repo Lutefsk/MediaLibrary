0e32da466edfe066a1b45c56418e970953838d39
Christina Kelm <CKelm1@my.wctc.edu>
2023-10-29 20:31:45 -0500

    Add Linq for 1990 movie count

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
 
+Console.ForegroundColor = ConsoleColor.Green;
+// LINQ - Where filter operator & Contains quantifier operator
+var Movies = movieFile.Movies.Where(m => m.title.Contains("(1990)"));
+// LINQ - Count aggregation method
+Console.WriteLine($"There are {Movies.Count()} movies from 1990");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+
 bool optionISNOT1or2 = true;
 while (optionISNOT1or2)
 {
